20716f3c2aed12cb8dd9ae12dac68f9f41b2b502
MKozubski <mkozubski@aceproject.space>
2023-11-02 17:52:45 -0500

    Fixed images so they display properly

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :carts
   has_many :users, through: :carts
   has_many :product_images, dependent: :destroy
+  has_one_attached :image
   accepts_nested_attributes_for :product_images, allow_destroy: true
 end
 
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -2,11 +2,6 @@ class ProductImage < ApplicationRecord
   # Association with the Product model
   belongs_to :product
 
-  # Assuming you're using something like CarrierWave, Paperclip, or Active Storage for image uploads:
   has_one_attached :image
 
-  # Validations and other custom logic (if necessary)
-  # For example, validate presence of image, file type, etc.
-  # validates :image, presence: true
-  # ... more custom logic
 end
