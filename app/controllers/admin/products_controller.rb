9e960406e0126f73cdb4d2644dfab3c838280bb1
MKozubski <mkozubski@aceproject.space>
2023-11-02 17:05:02 -0500

    added images

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,15 @@
 module Admin
   class ProductsController < ApplicationController
-    # before_action :require_admin
 
+    def log_p(variable)
+      Rails.logger.info("Variable value: #{variable}")
+    end
+    def log_pp(variable)
+      Rails.logger.info("Variable value: #{variable}")
+    end
+    def log_i(variable)
+      Rails.logger.info("Variable value: #{variable}")
+    end
     # List all products
     def index
       @products = Product.all
@@ -20,7 +28,9 @@ module Admin
     # Save a new product
     def create
       @product = Product.new(product_params)
+      log_p(product_params)
       if @product.save
+        handle_image(params[:product][:image]) if params.dig(:product, :image)
         redirect_to [:admin, @product], notice: 'Product was successfully created.'
       else
         render :new
@@ -35,7 +45,10 @@ module Admin
     # Update an existing product
     def update
       @product = Product.find(params[:id])
+
       if @product.update(product_params)
+        log_pp(product_params)
+        handle_image(params[:product][:image]) if params.dig(:product, :image)
         redirect_to [:admin, @product], notice: 'Product was successfully updated.'
       else
         render :edit
@@ -52,13 +65,20 @@ module Admin
     private
 
     def product_params
-      params.require(:product).permit(:product_name, :product_description, :product_price, :category_id)
+      params.require(:product).permit(:product_name, :product_description, :product_price, :category_id, :original_filename)
     end
 
-    # def require_admin
-    #   unless current_user&.is_admin?
-    #     redirect_to root_path, alert: 'You are not authorized to access this page.'
-    #   end
-    # end
+    def handle_image(uploaded_image)
+      log_i(uploaded_image)
+      return unless uploaded_image
+
+      # Attach the uploaded image to the product
+      new_image = @product.product_images.new(image: uploaded_image)
+
+      # Save the new record
+      unless new_image.save
+        Rails.logger.error "Failed to save image for product ##{@product.id}"
+      end
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :carts
   has_many :users, through: :carts
+  has_many :product_images, dependent: :destroy
+  accepts_nested_attributes_for :product_images, allow_destroy: true
 end
+
