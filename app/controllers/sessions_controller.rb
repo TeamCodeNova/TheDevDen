e3a1c8360994065e6a3f8da55922c512cc42e7cf
MKozubski <mkozubski@aceproject.space>
2023-11-08 12:43:52 -0600

    added user accounts

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,63 @@
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session # Protects from session fixation attacks
+      session[:user_id] = user.id
+      remember(user) # This method will be implemented to remember the user
+      redirect_to root_path, notice: 'Logged in successfully.'
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    forget(current_user) if logged_in? # This method will be implemented to forget the user
+    reset_session
+    redirect_to login_path, notice: 'Logged out successfully.'
+  end
+
+  private
+
+  # Remembers a user in a persistent session by setting a secure cookie.
+  def remember(user)
+    user.remember # This method will be added in the User model
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget # This method will be added in the User model
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Checks if a user is logged in.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Retrieves the current user from the session or cookies.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # Admin authentication routes
   get 'admin_login', to: 'admin_sessions#new'
   post 'admin_login', to: 'admin_sessions#create'
   delete 'admin_logout', to: 'admin_sessions#destroy'
+
+  get    'login',   to: 'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'users', to: 'users#create'
 
   # Other routes
   root 'home#index'
   resources :products, only: [:index, :show]
   resources :cart, only: [:index]
   resource :account, only: [:show]
+  resources :users, only: [:new, :create]
   namespace :admin do
     resources :products
   end
